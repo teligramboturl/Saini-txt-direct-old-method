f8dadd483b043ce0ccd123fae0e0c97b5b5a7692
teligramboturl <teligramboturl@gmail.com>
2025-09-05 23:25:43 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,13 +3,14 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "23673651"))
-API_HASH = environ.get("API_HASH", "f032bfa12ee46e1283f6fb23cfca5c6b")
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER = int(environ.get("OWNER", "6677821706"))
 CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
 LOG_CHANNEL = "-1002685378156" # log group me bot ko admin narur banaye full rights ke sath
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8080 if not set
+
 
 
